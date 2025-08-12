f80f34e25fe3c7e628aeb244bfe025d0a23cb11e
giacomopiccinini <piccinini.giacomo@gmail.com>
2025-08-12 08:58:12 +0200

    [INIT]

---
+++ src/db.rs
@@ -0,0 +1,38 @@
+use anyhow::{Context, Result};
+use sqlx::ConnectOptions;
+use sqlx::sqlite::{SqliteConnectOptions, SqlitePool};
+use std::str::FromStr;
+
+use std::path::PathBuf;
+use dirs;
+
+/// Get the connection string to SQLite DB
+fn get_db_connection_str() -> Result<String> {
+
+    // Use data directory to standardize storage
+    let data_dir = dirs::data_dir()
+        .with_context(|| "Unable to find data directory")?
+        .join("td");
+    
+    // Create directory if it doesn't exist
+    std::fs::create_dir_all(&data_dir).with_context(|| "Unable to create data directory")?;
+
+    Ok(format!("sqlite:{}", data_dir.join("todos.db").display()))
+
+}
+
+/// Create connection to SQLite DB pool and create DB if not present
+async fn get_db_pool(db_connection_str: str) -> Result<SqlitePool>{
+
+    // Create connection options
+    let opts = SqliteConnectOptions::from_str(db_connection_str)
+        .with_context(|| "Failed to create options for DB")?
+        .create_if_missing(true);
+
+    // Connect in a pool
+    let pool = SqlitePool::connect_with(opts)
+    .await
+    .with_context(|| "Failed to create DB pool")?;
+
+    Ok(pool)
+}
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use color_eyre::Result;
+use std::io;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame,
+};
+
+fn main() -> io::Result<()>{
+
+    // Set the terminal up
+    let mut terminal = ratatui::init();
+
+    // Crate and run the app
+    let app_result = App::default().run(&mut terminal);
+
+    // TODO
+    ratatui::restore();
+
+    // TODO
+    app_result
+}
+
+pub enum CurrentScreen {
+    Main,
+    Editing,
+    Exiting,
+}
+
+#[derive(Debug, Default)]
+pub struct App {
+    exit: bool,
+}
+
+impl App {
+
+    /// runs the application's main loop until the user quits
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    // /// Load saved to-do lists
+    fn load_db(&self){
+        todo!()
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    /// updates the application's state based on user input
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+}
+
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" To-Do ".bold());
+        let instructions = Line::from(vec![
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from("Some text");
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}

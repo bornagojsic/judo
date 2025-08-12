f7b3efedbd6b916cb406abe1dea1d52a019a197e
giacomopiccinini <piccinini.giacomo@gmail.com>
2025-08-12 09:02:37 +0200

    [INIT]

--- src/db.rs
+++ src/db.rs
@@ -1,26 +1,23 @@
 use anyhow::{Context, Result};
+use dirs;
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePool};
 use std::str::FromStr;
-use dirs;
 
 /// Get the connection string to SQLite DB
 fn get_db_connection_str() -> Result<String> {
-
     // Use data directory to standardize storage
     let data_dir = dirs::data_dir()
         .with_context(|| "Unable to find data directory")?
         .join("td");
-    
+
     // Create directory if it doesn't exist
     std::fs::create_dir_all(&data_dir).with_context(|| "Unable to create data directory")?;
 
     Ok(format!("sqlite:{}", data_dir.join("todos.db").display()))
-
 }
 
 /// Create connection to SQLite DB pool and create DB if not present
-async fn get_db_pool(db_connection_str: &str) -> Result<SqlitePool>{
-
+async fn get_db_pool(db_connection_str: &str) -> Result<SqlitePool> {
     // Create connection options
     let opts = SqliteConnectOptions::from_str(db_connection_str)
         .with_context(|| "Failed to create options for DB")?
@@ -28,8 +25,8 @@ async fn get_db_pool(db_connection_str: &str) -> Result<SqlitePool>{
 
     // Connect in a pool
     let pool = SqlitePool::connect_with(opts)
-    .await
-    .with_context(|| "Failed to create DB pool")?;
+        .await
+        .with_context(|| "Failed to create DB pool")?;
 
     Ok(pool)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use color_eyre::Result;
-use std::io;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
+    DefaultTerminal, Frame,
     buffer::Buffer,
     layout::Rect,
     style::Stylize,
     symbols::border,
     text::{Line, Text},
     widgets::{Block, Paragraph, Widget},
-    DefaultTerminal, Frame,
 };
+use std::io;
 mod db;
 
-fn main() -> io::Result<()>{
-
+fn main() -> io::Result<()> {
     // Set the terminal up
     let mut terminal = ratatui::init();
 
@@ -39,7 +38,6 @@ pub struct App {
 }
 
 impl App {
-
     /// runs the application's main loop until the user quits
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
@@ -50,7 +48,7 @@ impl App {
     }
 
     // /// Load saved to-do lists
-    fn load_db(&self){
+    fn load_db(&self) {
         todo!()
     }
 
@@ -83,14 +81,10 @@ impl App {
     }
 }
 
-
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(" To-Do ".bold());
-        let instructions = Line::from(vec![
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
+        let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
         let block = Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())

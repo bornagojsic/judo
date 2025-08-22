352a68f9fece832398118bde786e66f0d2dbad82
giacomopiccinini <piccinini.giacomo@gmail.com>
2025-08-22 09:00:45 +0200

    [FEAT] Ascii logo and layot

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # td
 TUI for to-do lists
+
+## Widgets
+
+- Canvas: for title (https://ratatui.rs/examples/widgets/canvas/)
+- List: for todolists (https://ratatui.rs/examples/widgets/list/)
+- Scrollbar: may be useful (https://ratatui.rs/examples/widgets/scrollbar/)
+- Table: for items (https://ratatui.rs/examples/widgets/table/)
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,197 @@
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    DefaultTerminal, Frame,
-    buffer::Buffer,
-    layout::Rect,
-    style::Stylize,
-    symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+use color_eyre::Result;
+use crossterm::event::{self, KeyCode, KeyEvent};
+use ratatui::buffer::Buffer;
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::style::Stylize;
+use ratatui::widgets::{
+    Block, Borders, HighlightSpacing, List, ListItem, ListState, Padding, Paragraph,
+    StatefulWidget, Widget, Wrap,
 };
-use std::io;
+use ratatui::{DefaultTerminal, symbols};
 
-fn main() -> io::Result<()> {
+// #[derive(Debug, Default)]
+pub struct App {
+    status: i32,
+    exit: bool,
+}
+
+// Default trait should "load" the initial state of the app
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            exit: false,
+            status: 42,
+        }
+    }
+}
+
+impl App {
+    /// Run the application
+    fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+            if let Some(key) = event::read()?.as_key_press_event() {
+                self.handle_key(key);
+            }
+        }
+        Ok(())
+    }
+
+    /// Handle key press from user
+    fn handle_key(&mut self, key: KeyEvent) {
+        match key.code {
+            KeyCode::Char('q') | KeyCode::Esc => self.exit = true,
+            _ => {}
+        }
+    }
+}
+
+/// Widget trait implements the high-level rendering logic
+/// Actual rendering functions to be implemented for App and not as part of a trait
+/// It should always implement render
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        // Define the main layout of the app
+        let main_layout = Layout::vertical([
+            Constraint::Percentage(20), // Header
+            Constraint::Percentage(70), // Main content
+            Constraint::Percentage(10), // Footer
+        ]);
+
+        // Extract the areas from the main layout
+        let [header_area, content_area, footer_area] = main_layout.areas(area);
+
+        // Further subdivide the content area into list area and item area
+        let content_layout =
+            Layout::horizontal([Constraint::Percentage(30), Constraint::Percentage(70)]);
+
+        // Extract the areas for lists and items
+        let [lists_area, items_area] = content_layout.areas(content_area);
+
+        // Render the four areas
+        App::render_header(header_area, buf);
+        App::render_footer(footer_area, buf);
+        self.render_lists(lists_area, buf);
+        self.render_items(items_area, buf);
+    }
+}
+
+impl App {
+    // Render the header
+    fn render_header(area: Rect, buf: &mut Buffer) {
+        // Use the td ascii logo
+        let ascii_logo = r#"
+████████╗  ██████═╗ 
+╚══██╔══╝  ██╔═══██╗
+   ██║     ██║   ██║
+   ██║     ██║   ██║
+   ██║     ██████╔═╝
+   ╚═╝     ╚═════╝
+        "#;
+
+        // Define a block and pad. Can't pad on a paragraph, so we need to insert the paragraph inside
+        // the padded block
+        let block = Block::default().padding(Padding::horizontal(2));
+
+        Paragraph::new(ascii_logo)
+            .bold()
+            .left_aligned()
+            .block(block)
+            .render(area, buf);
+    }
+
+    // Render footer with instructions
+    fn render_footer(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Use ↓↑ to move, ← to unselect, → to change status, g/G to go top/bottom.")
+            .centered()
+            .render(area, buf);
+    }
+
+    // Render list of items
+    fn render_lists(&mut self, area: Rect, buf: &mut Buffer) {
+        Paragraph::new("List area")
+            .bold()
+            .left_aligned()
+            .render(area, buf);
+    }
+
+    // Render list of items
+    fn render_items(&mut self, area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Item area")
+            .bold()
+            .left_aligned()
+            .render(area, buf);
+    }
+
+    // fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
+    //     let block = Block::new()
+    //         .title(Line::raw("TODO List").centered())
+    //         .borders(Borders::TOP)
+    //         .border_set(symbols::border::EMPTY)
+    //         .border_style(TODO_HEADER_STYLE)
+    //         .bg(NORMAL_ROW_BG);
+
+    //     // Iterate through all elements in the `items` and stylize them.
+    //     let items: Vec<ListItem> = self
+    //         .todo_list
+    //         .items
+    //         .iter()
+    //         .enumerate()
+    //         .map(|(i, todo_item)| {
+    //             let color = alternate_colors(i);
+    //             ListItem::from(todo_item).bg(color)
+    //         })
+    //         .collect();
+
+    //     // Create a List from all list items and highlight the currently selected one
+    //     let list = List::new(items)
+    //         .block(block)
+    //         .highlight_style(SELECTED_STYLE)
+    //         .highlight_symbol(">")
+    //         .highlight_spacing(HighlightSpacing::Always);
+
+    //     // We need to disambiguate this trait method as both `Widget` and `StatefulWidget` share the
+    //     // same method name `render`.
+    //     StatefulWidget::render(list, area, buf, &mut self.todo_list.state);
+    // }
+
+    // fn render_selected_item(&self, area: Rect, buf: &mut Buffer) {
+    //     // We get the info depending on the item's state.
+    //     let info = if let Some(i) = self.todo_list.state.selected() {
+    //         match self.todo_list.items[i].status {
+    //             Status::Completed => format!("✓ DONE: {}", self.todo_list.items[i].info),
+    //             Status::Todo => format!("☐ TODO: {}", self.todo_list.items[i].info),
+    //         }
+    //     } else {
+    //         "Nothing selected...".to_string()
+    //     };
+
+    //     // We show the list item's info under the list in this paragraph
+    //     let block = Block::new()
+    //         .title(Line::raw("TODO Info").centered())
+    //         .borders(Borders::TOP)
+    //         .border_set(symbols::border::EMPTY)
+    //         .border_style(TODO_HEADER_STYLE)
+    //         .bg(NORMAL_ROW_BG)
+    //         .padding(Padding::horizontal(1));
+
+    //     // We can now render the item info
+    //     Paragraph::new(info)
+    //         .block(block)
+    //         .fg(TEXT_FG_COLOR)
+    //         .wrap(Wrap { trim: false })
+    //         .render(area, buf);
+    // }
+}
+
+fn main() -> Result<()> {
     // Set the terminal up
     let mut terminal = ratatui::init();
 
     // Crate and run the app
     let app_result = App::default().run(&mut terminal);
 
-    // TODO
     ratatui::restore();
 
-    // TODO
     app_result
-}
-
-pub enum CurrentScreen {
-    Main,
-    Editing,
-    Exiting,
-}
-
-#[derive(Debug, Default)]
-pub struct App {
-    exit: bool,
-}
-
-impl App {
-    /// runs the application's main loop until the user quits
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
-        while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-            self.handle_events()?;
-        }
-        Ok(())
-    }
-
-    // /// Load saved to-do lists
-    fn load_db(&self) {
-        todo!()
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    /// updates the application's state based on user input
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        if let KeyCode::Char('q') = key_event.code {
-            self.exit()
-        }
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" To-Do ".bold());
-        let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        let counter_text = Text::from("Some text");
-
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
-    }
 }
